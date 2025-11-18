bbcfcde2e46cfa6d0b2a63e3f658bb2c19c5517f
asadbekabdulhamidov <aabdulhamidov818@gmail.com>
2025-11-18 09:29:13 +0500

    get all and get id books added

--- src/app.js
+++ src/app.js
@@ -12,6 +12,12 @@ app.use(cors()); // boshqa domen/ portlaridan kelgan sorovlarga ruhsat beradi ma
 app.use(helmet()); // xavfsizlik uchun HTTP headerlarni sozlaydi
 app.use(morgan('dev')); // har bir requestni konsolga log qilib boradi
 
+// Temporary "database" (memory-based)
+let books = [
+  { id: 1, title: 'Atomic Habits', author: 'James Clear' },
+  { id: 2, title: 'Clean Code', author: 'Robert Martin' },
+];
+
 //test api
 
 app.get('/', (req, res) => {
@@ -19,6 +25,35 @@ app.get('/', (req, res) => {
     status: 'ok',
     message: 'book Api ishlamoqda ',
   });
+});
+
+// get all api
+
+app.get('/api/books', (req, res) => {
+  res.json({
+    succes: true,
+    data: books,
+  });
+});
+
+// id boyicha bitta kitobni olish
+
+app.get('/api/books/:id', (req, res) => {
+  let bookId = Number(req.params.id);
+
+  let book = books.find((b) => b.id === bookId);
+
+  if (book) {
+    res.json({
+      book: book,
+      success: true,
+    });
+  } else {
+    res.status(404).json({
+      success: false,
+      message: 'Bunday kitob topilmadi',
+    });
+  }
 });
 
 // 3) app'ni tashqariga export qilamiz
